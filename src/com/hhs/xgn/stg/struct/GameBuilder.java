ae303ccb5cb63cb0de7d17a5acdcb3143ebf2030
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2019-12-29 11:25:20 +0800

    game builder core

---
+++ src/com/hhs/xgn/stg/struct/GameBuilder.java
@@ -0,0 +1,26 @@
+package com.hhs.xgn.stg.struct;
+
+import java.util.ArrayList;
+
+import com.hhs.xgn.stg.type.Player;
+
+/**
+ * A structure class to build a game 
+ * @author XGN
+ *
+ */
+public abstract class GameBuilder {
+	
+	/**
+	 * Stages
+	 */
+	public ArrayList<StageBuilder> stage;
+	/**
+	 * Players
+	 */
+	public ArrayList<Player> self;
+	/**
+	 * Difficulties
+	 */
+	public ArrayList<String> diffs;
+}
